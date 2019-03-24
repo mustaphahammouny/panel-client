2e93b807998910a8a49358e8de57c0da4a2b4f59
mustaphahammouny <mustapha.hammouny@gmail.com>
2019-03-24 15:50:39 +0100

    fix bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBffjg2aer2ZUm3hHLtvWUpkuba90y9-5c",
+    authDomain: "client-manager-d4cf6.firebaseapp.com",
+    databaseURL: "https://client-manager-d4cf6.firebaseio.com",
+    projectId: "client-manager-d4cf6",
+    storageBucket: "client-manager-d4cf6.appspot.com",
+    messagingSenderId: "991274201151"    
+  }
 };
